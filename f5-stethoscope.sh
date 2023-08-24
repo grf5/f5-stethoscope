77456479f6afdcf9bbc1e2357f1a95f0bb86c58f
Greg Robinson <g.robinson@f5.com>
2023-08-24 11:02:26 -0400

    disabling SSL cert check

--- f5-stethoscope.sh
+++ f5-stethoscope.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Disable self-signed certificate warning for BIG-IP
+export PYTHONWARNINGS='ignore:Unverified HTTPS request'
+
 robot --variable "host:10.1.1.4" --variable "user:admin" --variable "pass:f5c0nfig123!" f5-stethoscope.robot
