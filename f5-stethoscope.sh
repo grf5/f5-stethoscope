9f62213a43e3dc60596f28aa0615a42ff11c7c6a
Greg Robinson <g.robinson@f5.com>
2023-08-24 15:09:55 -0400

    removing bad char

--- f5-stethoscope.sh
+++ f5-stethoscope.sh
@@ -4,4 +4,4 @@
 export PYTHONWARNINGS='ignore:Unverified HTTPS request'
 
 export TIMESTAMP=$(date +'%Y-%m-%d_%H%M.%S')
-robot --variable  "text_output_file_name:device_info_$TIMESTAMP.txt" --variable "host:10.1.1.4" --variable "user:admin" --variable "pass:f5c0nfig123!" --logtitle "F5 Stethoscope $TIMESTAMP" --output-dir /usr/share/nginx/html -f5-stethoscope.robot
+robot --variable  "text_output_file_name:device_info_$TIMESTAMP.txta" --variable "host:10.1.1.4" --variable "user:admin" --variable "pass:f5c0nfig123!" --logtitle "F5 Stethoscope $TIMESTAMP" --output-dir /usr/share/nginx/html f5-stethoscope.robot
