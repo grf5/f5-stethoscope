dfc76763c69850592d16a0cca29681561ba5bffe
Greg Robinson <g.robinson@f5.com>
2023-09-11 20:25:25 -0400

    fixing escapes in bash

--- f5-stethoscope.sh
+++ f5-stethoscope.sh
@@ -5,10 +5,10 @@ export PYTHONWARNINGS='ignore:Unverified HTTPS request'
 
 export TIMESTAMP=$(date +'%Y-%m-%d_%H%M.%S')
 robot \
-  --variable "bigip_host:10.1.1.4"\
-  --variable "bigip_username:admin"\
-  --variable "bigip_password:f5c0nfig123!"\
-  --logtitle "F5 Stethoscope $TIMESTAMP"\
-  --output-dir /usr/share/nginx/html 
+  --variable "bigip_host:10.1.1.4"              \
+  --variable "bigip_username:admin"             \
+  --variable "bigip_password:f5c0nfig123!"      \
+  --logtitle "F5 Stethoscope $TIMESTAMP"        \
+  --output-dir /usr/share/nginx/html            \
   f5-stethoscope.robot
 
